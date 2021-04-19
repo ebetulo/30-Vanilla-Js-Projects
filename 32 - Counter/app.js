5a2436c126cb2fb4978abc8160228be99dd9b6a3
ebetulo <ozpolat-betul@hotmail.com>
2021-04-19 12:01:42 +0200

    Some comments are added.

--- 32 - Counter/app.js
+++ 32 - Counter/app.js
@@ -7,17 +7,22 @@ const buttons = document.querySelectorAll(".btn"); //Creates NodeList of 3 butto
 //Instead of selecting buttons one by one we are using ForEach method on NodeList!!
 buttons.forEach(function (item) {
   item.addEventListener("click", function (e) {
+    //const style = e.currentTarget.classList (There is no difference between 10 & 11)
     const style = e.target.classList;
     if (style.contains("decrease")) {
       count--;
     } else if (style.contains("increase")) {
       count++;
-    } else count = 0;
+    } else {
+      count = 0;
+    }
     if (count > 0) {
       value.style.color = "green";
     } else if (count < 0) {
       value.style.color = "red";
-    } else value.style.color = "black";
+    } else {
+      value.style.color = "black";
+    }
     value.textContent = count;
   });
 });
